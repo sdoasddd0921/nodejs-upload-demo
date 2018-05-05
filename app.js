53da55031f87c674b7762df051b17b3e01b876a7
ZQ-MBP <850307035@qq.com>
2018-05-05 21:07:03 +0800

    增加文件夹是否存在的检测

--- app.js
+++ app.js
@@ -1,18 +1,52 @@
-const path = require('path')
-const Koa = require('koa')
+const fs      = require('fs')
+const Koa     = require('koa')
+const path    = require('path')
 const koaBody = require('koa-body')
-const Router = require('koa-router')
-const server = require('koa-static')
-const multer = require('koa-multer')
+const Router  = require('koa-router')
+const server  = require('koa-static')
+const multer  = require('koa-multer')
+
+const uploadFolder = './uploads'
+
+// has uploads folder?
+function checkFolder() {
+  return async function (ctx, next) {
+    if (!fs.existsSync(uploadFolder)) {
+      fs.mkdirSync(uploadFolder)
+    }
+    if (next) {
+      await next()
+    }
+  }
+}
+checkFolder()()
 
 const app = new Koa()
-const upload = multer({ dest: './uploads'})
 const uploadRoute = new Router()
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    console.log(file);
+    cb(null, uploadFolder);
+  },
+  filename: function(req, file, cb) {
+    const originalnames = file.originalname.split('.')
+    const ext = '.' + originalnames.pop()
+    cb(null, originalnames + ext)
+  }
+});
+const upload = multer({ storage: storage })
 
-uploadRoute.post('/upload', upload.single('test'), async (ctx, next) => {
-  console.log(ctx.req.file)
-  ctx.body = 'got'
-})
+uploadRoute.post(
+  '/upload',
+  checkFolder(),
+  upload.single('test'),
+  async (ctx, next) => {
+    ctx.body = ctx.req.file
+    if (next) {
+      await next()
+    }
+  }
+)
 
 app
   .use(server(path.join(__dirname, '/static')))
